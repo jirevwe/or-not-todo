74afb15e4fcde596da69c9002d1daa91e5af23da
Raymond Tukpe <rtukpe@gmail.com>
2020-02-18 20:03:25 +0100

    hotfix: heroku needs the main entry point to be PORT and nothing else, UGH.

--- src/server/workers/bootstrap.ts
+++ src/server/workers/bootstrap.ts
@@ -4,13 +4,13 @@ import db from '../db';
 
 export const startWorker = async () => {
   try {
-    if (!env.worker_port)
+    if (!env.port)
       throw new Error('Worker http port not specified. Exiting...');
 
     await db.connect();
 
-    await bolt.start(env.worker_port);
-    console.log(`⚡️ Bolt app is running on port ${env.worker_port}!!!`);
+    await bolt.start(env.port);
+    console.log(`⚡️ Bolt app is running on port ${env.port}!!!`);
   } catch (e) {
     process.exit(1);
   }
